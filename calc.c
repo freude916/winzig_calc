64f8b61c32f78c4f8eec157181ea1bd58f4ff4a9
zyc916 <2142045058@qq.com>
2024-11-09 02:34:56 +0800

    Parser and Executor completed.

--- calc.c
+++ calc.c
@@ -1,9 +1,13 @@
 #pragma once
 #include <assert.h>
+#include <math.h>
 #include <stdlib.h>
 #include <string.h>
 
 #include "defs.h"
+#include "base.c"
+#include "tokenizer.c"
+#include "parser.c"
 // A Expression Calculator
 // can eval +-*/(), math function call, variable, assignment, simple loop, if-else, function definition and call
 
@@ -23,342 +27,125 @@
 * if, else, while, return
 */
 
-enum TokenType {
-    TokenNull, // used for state mark, don't use in real token
-    TokenWord, TokenNumber, TokenOperator, TokenNewLine,
-};
-
-enum Tag {
-    TNull, // nothing, only used in base
-    TNone, // nothing value, may appear in Expression?
-
-    TLiteral, TIdentifier,
-
-    TExpr2, TExpr1, TExpression,
-
-    TBuiltin, TPrint, TFunction,
-
-    TStatement,
-
-    TBlock,
-
-    TWhile, TIf,
-};
-
-struct Literal {
-    long double value;
-};
-
-struct Identifier {
-    char *name;
-};
-
-struct Expr2 {
-    struct Expression *lhs;
-    struct Expression *rhs;
-    char op;
-    // +, -, *, /, &&, ||, ==, !=, <, <=, >, >=, etc.
-    // =, +=, -=, *=, /=, %=, &=, |=, ^=, <<=, >>=, etc.
-};
-
-struct Expr1 {
-    struct Expression *expr;
-    char op;
-    // possible unary operators: +, -, !, ~, etc.
-};
-
-struct Expression {
-    enum Tag tag;
-
-    union {
-        struct Literal literal;
-        struct Identifier identifier;
-        struct Expr2 expr2;
-        struct Expr1 expr1;
-        struct None none;
-    };
-};
-
-struct Builtin {
-    double (*func)(double);
-
-    struct Expression expr;
-}; // abs, sin, cos, tan, asin, acos, atan, sqrt, log, log10, exp, ceil, floor, round, etc.
-
-// struct Print {
-//     char *message;
-//     struct Expression expr;
-// };
-// TODO Too difficult to implement, just ignore it
-
-struct Function {
-    struct Identifier func;
-    struct Expression *expr; // ends with TNull
-};
-
-struct Return {
-    struct Expression expr;
-};
-
-struct Block {
-    struct Statement *stmts; // ends with TNull
-};
-
-struct Statement {
-    enum Tag tag;
-
-    union {
-        struct Expression expr;
-        struct Builtin builtin;
-        // struct Print print;
-        struct Function function;
-        struct Return ret;
-        struct Block block;
-    };
-};
-
-struct While {
-    struct Expression cond;
-    struct Block block;
-};
-
-struct If {
-    struct Expression cond;
-    struct Block then_block;
-    struct Block else_block; // may be TNull
-};
-
-struct Token {
-    enum TokenType tag;
-    char *token;
-};
-
-struct TokenData {
-    struct Token *tokens; // should end with TNull "\0", please push in the end.
-    int count;
-    int size; // using in malloc or reallocate
-    int error; // 0 for no err, 1 for grammar, 2 for invalid char, -1 for internal error
-    int index; // current index, pop
-};
-
-
-struct TokenData *Ts_create() {
-    struct TokenData *tokens = malloc(sizeof(struct TokenData));
-    tokens->tokens = malloc(sizeof(struct Token) * INIT_TOKEN_COUNT);
-    tokens->count = 0;
-    tokens->size = INIT_TOKEN_COUNT;
-    tokens->error = 0;
-    tokens->index = 0;
-    return tokens;
+int string_hash(const char* str) {
+    int hash = 5381;
+    while (*str) {
+        hash = (hash * 31 + *str) % 65536;
+        str++;
+    }
+    return hash;
 }
 
-void Ts_push(struct TokenData *tokens, enum Tag tag, const char *const token, int token_len) {
-    if (!tokens) {
-        return;
-    }
-    if (token_len <= 0) {
-        token_len = strlen(token);
-    }
-    if (tokens->count >= tokens->size) {
-        tokens->size *= 2;
-        tokens->tokens = realloc(tokens->tokens, sizeof(struct Token) * tokens->size);
-    }
-
-    tokens->tokens[tokens->count].tag = tag;
-    tokens->tokens[tokens->count].token = malloc((token_len + 1) * sizeof(char));
-    strcpy(tokens->tokens[tokens->count].token, token);
-    tokens->count++;
-}
-
-void Ts_end(struct TokenData *tokens) {
-    Ts_push(tokens, TokenNull, "", 1);
-}
-
-void Ts_delete(struct TokenData *tokens) {
-    free(tokens->tokens);
-    free(tokens);
-}
-
-struct Token Ts_pop(struct TokenData *tokens) {
-    if (tokens->index >= tokens->count) {
-        tokens->error = -1; // internal error
-        assert(0); // should not happen, we had push a TokenNull in the end before
-    }
-    return tokens->tokens[tokens->index++];
-}
-
-/**
- * Tokenize the source code
- *
- * @param src the source code, better with space between tokens, if not also OK
- * @return a list of tokens (ends with "\0")
- */
-struct TokenData *tokenize(const char *src) {
-    /// OK
-    /// Passed test on 2024-11-06 22:07
-    enum TokenType state = TokenNull;
-    // Null for initial or blank, Word for word, Number for number, Operator for operator
-
-    struct TokenData *tokens = Ts_create();
-    char *token = malloc(sizeof(char) * MAX_TOKEN_LEN); // assume never exceed MAX_TOKEN_LEN. what hell can be so long?
-    int token_len = 0;
-#define PUSH_CHAR(c) token[token_len++] = c
-#define PUSH_TOKEN(tag) \
-    if (token_len > 0){\
-        ;\
-        PUSH_CHAR('\0');\
-        Ts_push(tokens, tag, token, token_len);\
-        token_len = 0;\
-    }
-
-    if (src == NULL || *src == '\0') {
-        Ts_end(tokens);
-        return tokens;
-    }
-    do {
-        if (*src == ' ' || *src == '\t') {
-            PUSH_TOKEN(state);
-            state = TokenNull;
-            continue;
-        }
-        if (*src == '\n' || *src == '\r') {
-            PUSH_TOKEN(state);
-            PUSH_TOKEN(TokenNewLine);
-            while (*src == '\n' || *src == '\r') src++;
-            state = TokenNull;
-        }
-        if (*src >= '0' && *src <= '9') {
-            if (state == TokenOperator && token_len == 1 && (token[0] == '+' || token[0] == '-')) {
-                PUSH_CHAR(*src);
-                state = TokenNumber;
-                continue;
-            }
-            if (state != TokenNull && state != TokenNumber) {
-                PUSH_TOKEN(state);
-            }
-            PUSH_CHAR(*src);
-            state = TokenNumber;
-            continue;
-        }
-        if ((*src >= 'a' && *src <= 'z') || (*src >= 'A' && *src <= 'Z')) {
-            if (state != TokenNull && state != TokenWord) {
-                PUSH_TOKEN(state);
-            }
-            PUSH_CHAR(*src);
-            state = TokenWord;
-            continue;
-        }
-        /**
-         * possible operator char
-         * + - * / = > < & | !
-         */
-        if (*src == '+' || *src == '-' || *src == '*' || *src == '/' || *src == '^' ||
-            *src == '>' || *src == '<' || *src == '=' ||
-            *src == '&' || *src == '|' || *src == '!') {
-            if (state != TokenNull && state != TokenOperator) {
-                PUSH_TOKEN(state);
-            }
-            if (*src == '=') {
-                // all operator has a version with '=' :)
-                PUSH_CHAR(*src);
-                PUSH_TOKEN(TokenOperator);
-                state = TokenNull;
-            } else {
-                if (state == TokenOperator) {
-                    tokens->error = 1; // grammar error. two operator in a row
-                    break;
-                }
-                PUSH_CHAR(*src);
-                state = TokenOperator;
-            }
-            continue;
-        }
-        if (*src == '(' || *src == ')' || *src == '{' || *src == '}') {
-            if (state != TokenNull) {
-                PUSH_TOKEN(state);
-            }
-            PUSH_CHAR(*src);
-            PUSH_TOKEN(TokenOperator);
-            state = TokenNull;
-            continue;
-        }
-        tokens->error = 2; // invalid charactor
-        break;
-    } while (*++src != '\0');
-    PUSH_TOKEN(state);
-    Ts_end(tokens);
-    return tokens;
-#undef PUSH_CHAR
-#undef PUSH_TOKEN
-}
-
-/**
- * Priority Table
- * Top
- * 1 ( )
- * 2 !
- * 3 ^ (pow)
- * 4 * /
- * 5 + -
- * 6 < <= > >=
- * 7 == !=
- * 8 & (logical and)
- * 9 | (logical or)
- * 10 = += -= *= /= ^= (calculate then assign)
- * 10 &= |= (logic then assign)
- */
-
-int operator_priority(char *op) {
-    if (*op == '\0') {
-        return -1; // should not happen
-    }
-    if (op[1] == '=') {
+long double calc(long double a, long double b, char *op) {
+    if (op[1] != '\0') {
         switch (*op) {
-            case '+':
-            case '-':
-            case '*':
-            case '/':
-            case '^':
-            case '&':
-            case '|':
-                return 10;
-            case '=':
-            case '!':
-                return 7;
-            case '<':
-            case '>':
-                return 6;
-            default:
-                return -1; // should not happen
+            case '>': return a >= b;
+            case '<': return a <= b;
+            case '=': return a == b;
+            case '!': return a != b;
+            default:  assert(0); // should not reach here
         }
     }
     switch (*op) {
-        case '(':
-            case ')':
-        return 1;
-        case '!':
-            return 2;
-        case '^':
-            return 3;
-        case '*':
-            case '/':
-            return 4;
-        case '+':
-            case '-':
-            return 5;
-        case '<':
-            case '>':
-            return 6;
-        // all 7 was processed above =
-        case '&':
-            return 8;
-        case '|':
-            return 9;
-        default:
-            return -1; // should not happen
+        case '+': return a + b;
+        case '-': return a - b;
+        case '*': return a * b;
+        case '/': return a / b;
+        case '%': return fmod(a, b);
+        case '^': return pow(a, b);
+        case '&': return (long long)a & (long long)b;
+        case '|': return (long long)a | (long long)b;
+        case '>': return a > b;
+        case '<': return a < b;
+        default: assert(0); // should not reach here
     }
 }
 
-struct Expression* parse_expression(struct TokenData* tokens) {
+struct Executor {
+    long double variables[65536]; // hash set, except to save 256 elements
+    int error; // 0 for no err, 3 for runtime error
+};
 
+struct Executor* Executor_create() {
+    struct Executor* executor = malloc(sizeof(struct Executor));
+    memset(executor->variables, 0, sizeof(executor->variables));
+    executor->error = 0;
+    return executor;
+}
+
+long double Executor_get(struct Executor* executor, const char* name) {
+    return executor->variables[string_hash(name)];
+}
+
+void Executor_set(struct Executor* executor, const char* name, long double value) {
+    executor->variables[string_hash(name)] = value;
+}
+
+long double Executor_eval_Expression(struct Executor *executor, struct Expression *expr) {
+    if (expr -> tag == TLiteral) {
+        return expr -> literal -> value;
+    } if (expr -> tag == TIdentifier) {
+        return Executor_get(executor, expr -> identifier -> name); // the assignment should be done previously
+    } if (expr -> tag == TBuiltin) {
+        const long double value = Executor_eval_Expression(executor, expr -> builtin -> expr);
+        return expr -> builtin -> func(value);
+    } if (expr -> tag == TExpr2) {
+        struct Expr2 *expr2 = expr -> expr2;
+        if ((expr2->op[0] == '=' && expr2->op[1] == '\0') || (expr2->op[1] == '='  && expr2->op[0] != '=' && expr2->op[0] != '<' && expr2->op[0] != '>')) {
+            if (expr2->lhs->tag == TIdentifier) {
+                const long double value = Executor_eval_Expression(executor, expr2->rhs);
+                if (expr2->op[1] == '=') {
+                    // calc then assign
+                    char op[2] = {expr2->op[0], '\0'};
+                    long double before = Executor_get(executor, expr2->lhs->identifier->name);
+                    long double after = calc(before, value, op);
+                    Executor_set(executor, expr2->lhs->identifier->name, after);
+                    return after;
+                } else {
+                    Executor_set(executor, expr2->lhs->identifier->name, value);
+                    return value;
+                }
+            } else {
+                executor->error = 3;
+                return 0;
+            }
+        } else {
+            return calc(Executor_eval_Expression(executor, expr2->lhs), Executor_eval_Expression(executor, expr2->rhs), expr2->op);
+        }
+    }
+    // TODO: implement other tags
+    assert(0);
+}
+
+void Executor_eval_Block(struct Executor *executor, struct Block *block) ;
+
+void Executor_eval_Statement(struct Executor *executor, struct Statement *stmt) {
+    if (stmt -> tag == TExpression) {
+        Executor_eval_Expression(executor, stmt -> expr);
+        return;
+    } if (stmt -> tag == TIf) {
+        const long double condition = Executor_eval_Expression(executor, stmt -> if_stmt -> cond);
+        if (condition < eps) {
+            Executor_eval_Block(executor, stmt -> if_stmt -> else_block);
+        } else {
+            Executor_eval_Block(executor, stmt -> if_stmt -> then_block);
+        }
+    } if (stmt -> tag == TWhile) {
+        while (Executor_eval_Expression(executor, stmt -> while_stmt -> cond) > eps) {
+            Executor_eval_Block(executor, stmt -> while_stmt -> block);
+        }
+    }
+}
+
+void Executor_eval_Block(struct Executor *executor, struct Block *block) {
+    struct Statement* stmt = block -> stmts;
+    while (stmt -> tag != TNull) {
+        Executor_eval_Statement(executor, stmt);
+        stmt++;
+    }
+}
+
+void Executor_evaluate(struct Executor *executor, struct Block *block) {
+    Executor_eval_Block(executor, block);
+    Block_delete(block);
 }
